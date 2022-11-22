07eea91b9782a493d831e5dc028f7d97f1bec8e1
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2022-11-21 22:43:08 -0300

    2ª Atualização 21/11/2022

--- untitled/src/parser/PurchaseCsvParser.java
+++ untitled/src/parser/PurchaseCsvParser.java
@@ -1,20 +1,50 @@
 package parser;
 
+import entities.Product;
 import entities.Purchase;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class PurchaseCsvParser implements CsvParser <Purchase> {
 
+    private final String filePath;
+    private final String columnSymbol;
 
-    @Override
-    public void append(Purchase entity) throws IOException {
+    public PurchaseCsvParser(String filePath, String columnSymbol) {
+        this.filePath = filePath;
+        this.columnSymbol = columnSymbol;
+    }
 
+    String entityToString(Purchase purchase) {
+        return purchase.getId() + columnSymbol
+                + purchase.getUser().getId() + columnSymbol
+                + purchase.getProduct().getId();
     }
 
     @Override
-    public void storeAll(List<Purchase> entities) throws IOException {
+    public void append(Purchase purchase) throws IOException {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true))) {
+            String line = entityToString(purchase);
+            bw.write(line);
+            bw.newLine();
+        }
+    }
+
+
+
+    @Override
+    public void storeAll(List<Purchase> purchases) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))){
+              for (Purchase purchase: purchases){
+                bw.write(entityToString(purchase));
+                bw.newLine();
+            }
+
+
+        }
 
     }
 
